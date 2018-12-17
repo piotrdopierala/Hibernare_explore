9f6f8b52e3667985143c95432a190b93facf03e5
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-12-17 21:13:46 +0100

    - basic Hibernate transaction, add POJO instance to table in MySQL

---
+++ src/main/java/pl/dopierala/hibernatedemo1/SaveEntityApp.java
@@ -0,0 +1,37 @@
+package pl.dopierala.hibernatedemo1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import pl.dopierala.hibernatedemo1.entity.Employee;
+
+public class SaveEntityApp {
+    public static void main(String[] args) {
+        //tworzenie obiektu configuration
+        Configuration cfg = new Configuration();
+        //wczytanie pliku koknfiguracyjnego
+        cfg.configure("hibernate.cfg.xml");
+        //wczytanie adnotacji
+        cfg.addAnnotatedClass(Employee.class);
+        //stworzenie obiektu Session Factory
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        //pobieranie sesji
+        Session currentSession = sessionFactory.getCurrentSession();
+        //storzenie obiektu
+        Employee emp = new Employee();
+        emp.setIdEmployee(1);
+        emp.setFirstName("Jan");
+        emp.setLastName("Kowalski");
+        emp.setSalary(10000);
+        //rozpoczecie tranzakcji
+        currentSession.beginTransaction();
+        //zapisanie pracownika
+        currentSession.save(emp);
+        //zakonczenie transakcji
+        currentSession.getTransaction().commit();
+        //zamkniecie obiektu Session Factory
+        sessionFactory.close();
+
+
+    }
+}
